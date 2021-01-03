38315501102e5b8f2be74c6a2dde85f9b57eb237
ribbons almark <qy.wings@gmail.com>
2021-01-03 23:27:02 +0800

    commit

--- question/greedy/q0135/Solution.go
+++ question/greedy/q0135/Solution.go
@@ -10,7 +10,32 @@ package main
 // 那么这样下来，老师至少需要准备多少颗糖果呢？
 
 func candy(ratings []int) int {
-	return 0
+	var length = len(ratings)
+	var arr = make([]int, length, length)
+	for i := 0; i < length; i++ {
+		arr[i] = 1
+	}
+	for i := 1; i < length; i++ {
+		if ratings[i] > ratings[i-1] {
+			arr[i] = arr[i-1] + 1
+		}
+	}
+	for i := length - 2; i >= 0; i-- {
+		if ratings[i] > ratings[i+1] {
+			arr[i] = max(arr[i], arr[i+1]+1)
+		}
+	}
+	result := 0
+	for i := 0; i < length; i++ {
+		result += arr[i]
+	}
+	return result
+}
+func max(x, y int) int {
+	if x >= y {
+		return x
+	}
+	return y
 }
 func main() {
 
--- question/greedy/q0406/Solution.go
+++ question/greedy/q0406/Solution.go
@@ -1,11 +1,21 @@
 package main
 
+import "sort"
+
 // 假设有打乱顺序的一群人站成一个队列，数组 people 表示队列中一些人的属性（不一定按顺序）。每个 people[i] = [hi, ki] 表示第 i 个人的身高为 hi ，前面 正好 有 ki 个身高大于或等于 hi 的人。
 
 // 请你重新构造并返回输入数组 people 所表示的队列。返回的队列应该格式化为数组 queue ，其中 queue[j] = [hj, kj] 是队列中第 j 个人的属性（queue[0] 是排在队列前面的人）。
 
 func reconstructQueue(people [][]int) [][]int {
-
+	sort.Slice(people, func(i, j int) bool {
+		a, b := people[i], people[j]
+		return a[0] < b[0] || a[0] == b[0] && a[1] > b[1]
+	})
+	for i, p := range people {
+		copy(people[p[1]+1:i+1], people[p[1]:i])
+		people[p[1]] = p
+	}
+	return people
 }
 func main() {
 
