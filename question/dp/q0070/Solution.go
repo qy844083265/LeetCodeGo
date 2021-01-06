8e5d5f7a5f9adbd2ead60ea0b7046f314e9ac43c
ribbons almark <qy.wings@gmail.com>
2021-01-06 22:53:16 +0800

    comit

---
+++ question/dp/q0070/Solution.go
@@ -0,0 +1,22 @@
+package main
+
+// 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+func climbStairs(n int) int {
+	if n <= 2 {
+		return n
+	}
+	var dp = make([]int, 2)
+	dp[0] = 1
+	dp[1] = 2
+	for i := 2; i <= n; i++ {
+		sum := dp[0] + dp[1]
+		dp[0] = dp[1]
+		dp[1] = sum
+	}
+	return dp[0]
+}
+func main() {
+
+}
