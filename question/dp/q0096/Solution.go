0024e5d875580b840284a0dc53339a35bfc87c50
ribbons almark <qy.wings@gmail.com>
2021-01-15 00:02:25 +0800

    commit

---
+++ question/dp/q0096/Solution.go
@@ -0,0 +1,16 @@
+package main
+
+//给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+func numTrees(n int) int {
+	var dp = make([]int, n+1)
+	dp[0] = 1
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+	return dp[n]
+}
+func main() {
+
+}
