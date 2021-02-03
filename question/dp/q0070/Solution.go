4b66e6cf1da65f1f14e1d3a90e36969ddb94eb66
ye.qin <ye.qin@china.zhaogang.com>
2021-02-03 18:01:18 +0800

    commit

--- question/dp/q0070/Solution.go
+++ question/dp/q0070/Solution.go
@@ -17,6 +17,19 @@ func climbStairs(n int) int {
 	}
 	return dp[0]
 }
+func climbStairs1(n int) int {
+	m := 2
+	var dp = make([]int, n+1)
+	dp[0] = 1
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			if i-j >= 0 {
+				dp[i] += dp[i-j]
+			}
+		}
+	}
+	return dp[n]
+}
 func main() {
 
 }
