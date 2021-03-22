238be034c50b012e2d3511cb3204b2da9f7b4bd9
ye.qin <ye.qin@china.zhaogang.com>
2021-03-22 17:16:22 +0800

    commit

---
+++ question/dp/q0121/Solution.go
@@ -0,0 +1,35 @@
+package main
+
+/**
+ * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ * <p>
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+ * <p>
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+ */
+
+func maxProfit(prices []int) int {
+	var len = len(prices)
+	var dp = make([][]int, len)
+	for k, _ := range dp {
+		dp[k] = make([]int, 2)
+	}
+	dp[0][0] = -prices[0]
+	dp[0][1] = 0
+	for i := 1; i < len; i++ {
+		dp[i][0] = max(dp[i-1][0], -prices[i])
+		dp[i][1] = max(dp[i-1][1], prices[i]+dp[i-1][0])
+	}
+	return dp[len-1][1]
+}
+
+func max(x, y int) int {
+	if x >= y {
+		return x
+	}
+	return y
+}
+
+func main() {
+
+}
