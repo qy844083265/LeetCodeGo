e5b61490c3c9ddf914131c44cec69d8d1344f22c
ye.qin <ye.qin@china.zhaogang.com>
2021-03-23 16:39:36 +0800

    commit

---
+++ question/dp/q0123/Solution.go
@@ -0,0 +1,42 @@
+package main
+
+/**
+ * 给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+ * <p>
+ * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易。
+ * <p>
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ */
+func maxProfit(prices []int) int {
+	length := len(prices)
+	if length <= 0 {
+		return 0
+	}
+	var dp = make([][]int, length)
+	for k, _ := range dp {
+		dp[k] = make([]int, 5)
+	}
+	dp[0][0] = 0
+	dp[0][1] = -prices[0]
+	dp[0][2] = 0
+	dp[0][3] = -prices[0]
+	dp[0][4] = 0
+	for i := 1; i < length; i++ {
+		dp[i][0] = dp[i-1][0]
+		dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i])
+		dp[i][2] = max(dp[i-1][2], dp[i-1][1]+prices[i])
+		dp[i][3] = max(dp[i-1][3], dp[i-1][2]-prices[i])
+		dp[i][4] = max(dp[i-1][4], dp[i-1][3]+prices[i])
+	}
+	return dp[length-1][4]
+}
+
+func max(x, y int) int {
+	if x >= y {
+		return x
+	}
+	return y
+}
+func main() {
+
+}
